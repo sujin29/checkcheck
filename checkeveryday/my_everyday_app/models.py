70a85faead98642eebbe969fb719ff27055ad1eb
sujin <q9q989@gmail.com>
2021-01-01 19:22:50 +0900

    Add Foreign key, Add method at model

--- checkeveryday/my_everyday_app/models.py
+++ checkeveryday/my_everyday_app/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+from datetime import date
+from django.utils import timezone
 
 # Create your models here.
 class Habbit(models.Model):
     hab_title = models.CharField(max_length=255, null=False)
-    hab_record = models.CharField(max_length=255, null=False)
+    hab_method = models.CharField(max_length=10, null=False, default='ox')
+
+class CheckHab(models.Model):
+    check_title = models.ForeignKey(Habbit, on_delete=models.SET_DEFAULT, default='0')
+    check_method = models.CharField(max_length=10, null=False, default='ox')
+    check_date = models.DateField(max_length=100, default=timezone.now)
+    check_hour = models.IntegerField()
--- checkeveryday/my_everyday_app/views.py
+++ checkeveryday/my_everyday_app/views.py
@@ -8,13 +8,17 @@ def index(request):
     return render(request, "my_everyday_app/index.html")
 
 def createHabbit(request):
-    user_input_str = request.POST['habTitle']
-    new_habbit = Habbit(hab_title = user_input_str)
+    habbit_title = request.POST['habTitle']
+    habbit_method = request.POST['habMethod']
+
+    new_habbit = Habbit(hab_title = habbit_title, hab_method= habbit_method)
+
     new_habbit.save()
     return HttpResponseRedirect(reverse('dashboard'))
     # return HttpResponse('create Habbit 할거야! ->'+user_input_str)
 
 def dashboard(request):
     habbits = Habbit.objects.all()
-    content = {'habbits' : habbits}
+    # checks = CheckHab.objects.all()
+    content = {'habbits': habbits}
     return render(request, 'my_everyday_app/dashboard.html', content)
